b4dd4b215bde62ec957895e6498cfcd44b74fe14
Ruslan K <rus-blood@yandex.ru>
2021-11-13 01:39:59 +0300

    Using application context.

--- src/main/java/ru/guru/di/DiApplication.java
+++ src/main/java/ru/guru/di/DiApplication.java
@@ -2,12 +2,17 @@ package ru.guru.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.guru.di.controllers.MyController;
 
 @SpringBootApplication
 public class DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String text = myController.sayHello();
+		System.out.println(text);
 	}
 
 }
